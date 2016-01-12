f1d33da5dc8564b2fade6182bcb33cd3b7b1406a
Zach <zachnigels@gmail.com>
2016-01-11 22:06:09 -0500

    Robot Map
    Maps all of the static values for Input Output and Controllers

---
+++ src/org/usfirst/frc/team79/robot/RobotMap.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team79.robot;
+
+public class RobotMap {
+
+	// Controllers for Joysticks
+	
+	public static int LEFTCONTROLLER = 0;
+	public static int RIGHTCONTROLLER = 1;
+	
+	
+	//	......(\_/)
+	//	......( '_')
+	//	..../""""""""""""\======░ ▒▓▓█D
+	//	/"""""""""""""""""""""""\
+	//	\_@_@_@_@_@_/
+	
+	public static int MOTORLEFT = 0;
+	public static int MOTORRIGHT = 1;
+	
+}
