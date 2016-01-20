eb426e59ef6a1e03ac0fa87f23dfb3c4aa624699
Zach <zachnigels@gmail.com>
2016-01-19 22:16:46 -0500

    Update Teleop.java

--- src/org/usfirst/frc/team79/robot/commands/Teleop.java
+++ src/org/usfirst/frc/team79/robot/commands/Teleop.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class Teleop extends CommandGroup {
 
 	public Teleop() {
-		addParallel(new TankDrive());
+		addParallel(new Drive());
 	}
 	
 }
