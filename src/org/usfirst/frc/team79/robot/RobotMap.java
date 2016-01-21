57940de78937f7e3745c4be945bfeb7d56e12681
ammhist <orionwolfe00@gmail.com>
2016-01-20 21:31:12 -0500

    Update RobotMap.java

--- src/org/usfirst/frc/team79/robot/RobotMap.java
+++ src/org/usfirst/frc/team79/robot/RobotMap.java
@@ -1,20 +1,46 @@
 package org.usfirst.frc.team79.robot;
 
 public class RobotMap {
+//Joy sticks
+ public static int DRIVESTICKLEFT = 0;
+ public static int DRIVESTICKRIGHT = 1; 
+ public static int ARCBUTTON= 3;
+ 
+ //Motor Ports
+ public static int MOTORLFRONT = 0;
+ public static int MOTORLMIDDLE = 1;
+ public static int MOTORLBACK = 2;
+ public static int MOTORRFRONT = 3;
+ public static int MOTORRMIDDLE = 4;
+ public static int MOTORRBACK = 5;
+ public static int WINCHLEFT = 6;//PRAISE OUR LORD AND SAVIOR THE WINCH
+ public static int WINCHRIGHT = 7;//HE COMES AGAIN TO BLESS THIS CODE!!!
+ 
+ 
+ //DIO ports
+ public static int LEFTOE1 = 0;//Left Drivetrain OE
+ public static int LEFTOE2 = 1;//Second Left Drivetrain OE
+ public static int RIGHTOE1 = 2;//Right Drivetrain OE
+ public static int RIGHTOE2 = 3;//Second Right Drivetrain OE
+ public static int FRONTBS = 4;//aint this a bunch of banner sensor
+ public static int BACKBS = 5;//FROM THE BACK!!!!
+ public static int INTROLLERL = 9;
+ public static int INTROLLERR = 10; 
+ public static int SHOOTERROLLERL = 11;
+ public static int SHOOTERROOLERR = 12;
+ public static int FWEDGEACT = 12;
+ //Currently not used DIO Ports
+ public static int SHOOTEROE1 = 6; //OE is an optical encoder. use your EYESSSSS
+ public static int SHOOTEROE2 = 7; //OE is... look above you, you lazy coder
+ public static int LIMITSWITCH1 = 8;//ITS A LIMIT SWITCH!!!
+ public static int LIMITSWITCH2 = 9;//ITS ANOTHER LIMIT SWITCH!!!
 
-	// Controllers for Joysticks
-	
-	public static int LEFTCONTROLLER = 0;
-	public static int RIGHTCONTROLLER = 1;
-	
-	
-	//	......(\_/)
-	//	......( '_')
-	//	..../""""""""""""\======░ ▒▓▓█D
-	//	/"""""""""""""""""""""""\
-	//	\_@_@_@_@_@_/
-	
-	public static int MOTORLEFT = 0;
-	public static int MOTORRIGHT = 1;
-	
+ 
+ public static void init() {
+  // TODO Auto-generated method stub
+  
+ }
+ 
+ public static void loadCSVSettings(){
+ }
 }
