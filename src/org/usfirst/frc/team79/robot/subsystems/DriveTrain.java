f22f886adfcea92f815645487bd9a9d5cbea932f
ammhist <orionwolfe00@gmail.com>
2016-01-20 21:38:29 -0500

    Update DriveTrain.java

--- src/org/usfirst/frc/team79/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team79/robot/subsystems/DriveTrain.java
@@ -1,29 +1,44 @@
 package org.usfirst.frc.team79.robot.subsystems;
-
 import org.usfirst.frc.team79.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class DriveTrain extends Subsystem {
+ 
+ RobotDrive drivefmotors;
+ RobotDrive drivemmotors;
+ RobotDrive drivebmotors;
+ public static boolean isArcDrive;
+ public static Command setArcTrue; 
+ public DriveTrain() {
+  drivefmotors = new RobotDrive(RobotMap.MOTORRFRONT,RobotMap.MOTORLFRONT);// ITS THE FRONT!!!
+  drivemmotors = new RobotDrive(RobotMap.MOTORRMIDDLE,RobotMap.MOTORLMIDDLE);//ITS THE MIDDLE!!!
+  drivebmotors = new RobotDrive(RobotMap.MOTORRBACK,RobotMap.MOTORLBACK);//ITS THE BACK!!!!!
+ }//you cannot stop these amazing commenting skillz
 	
-	RobotDrive drive;
-	
-	public DriveTrain() {
-		drive = new RobotDrive(RobotMap.MOTORLEFT, RobotMap.MOTORRIGHT);
-	}
-	
-	public void moveTank(double left, double right) {
-		drive.tankDrive(left, right);
-	}
-	
-	public void moveArcade(double moveValue, double rotateValue) {
-		drive.arcadeDrive(moveValue, rotateValue);
-	}
 
-	@Override
-	protected void initDefaultCommand() {
-		
-	}
+ public void moveTD(double left, double right) {
+
+  drivefmotors.tankDrive(left, right);//YAY 3 DRIVES!!!
+  drivemmotors.tankDrive(left, right);
+  drivebmotors.tankDrive(left, right);
+ }//TAKE THIS BRACKET U CODE!!! HAAAHAHAHAHAHAHA
+ 
+	 public void moveAD(double power,double rotate) {
+			  drivefmotors.arcadeDrive(power, rotate);
+			  drivemmotors.arcadeDrive(power, rotate);
+			  drivebmotors.arcadeDrive(power, rotate);
+	 }
+			  //i am an mlc pro
+ public void stop() {
+  moveTD(0,0);
+  moveAD(0,0);
+ }//Pro major league commenter
+ @Override
+ protected void initDefaultCommand() {
+ }
+}
 
 }
