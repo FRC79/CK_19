446e52edb634440565a1b0e69177b8b87ec1762a
ammhist <orionwolfe00@gmail.com>
2016-01-20 21:29:28 -0500

    Update OI.java

--- src/org/usfirst/frc/team79/robot/OI.java
+++ src/org/usfirst/frc/team79/robot/OI.java
@@ -1,30 +1,24 @@
 package org.usfirst.frc.team79.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
-
 public class OI {
 	
-	public static Joystick left, right;
-	public static JoystickButton toggle;
+	public static Joystick left;
+	public static Joystick right;
+	public static JoystickButton arcButton;
 	
 	public static void init() {
-		left = new Joystick(RobotMap.LEFTCONTROLLER);
-		right = new Joystick(RobotMap.RIGHTCONTROLLER);
-		toggle = new JoystickButton(left, 0);
+		
+		left = new Joystick(RobotMap.DRIVESTICKLEFT);
+		right = new Joystick(RobotMap.DRIVESTICKRIGHT);
+		arcButton = new JoystickButton(left, RobotMap.ARCBUTTON);
 	}
-	
-	public static double getLeft() {
-		return left.getY();
-	}
-	
-	public static double getRight() {
-		return right.getY();
-	}
+}
 	
 	public static double getX() {
 		return left.getX();
