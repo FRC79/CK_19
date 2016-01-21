9e638605b81e825d38a0351cef18b6d4a5fbe96d
ammhist <orionwolfe00@gmail.com>
2016-01-20 21:30:16 -0500

    Update Robot.java

--- src/org/usfirst/frc/team79/robot/Robot.java
+++ src/org/usfirst/frc/team79/robot/Robot.java
@@ -1,30 +1,61 @@
-package org.usfirst.frc.team79.robot;
-
-import org.usfirst.frc.team79.robot.commands.CommandBase;
-import org.usfirst.frc.team79.robot.commands.Teleop;
-
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
+ SendableChooser arcadetrue;
+  SendableChooser arcadefalse;
+  SendableChooser auntonselector;
+  
+    Command enabled;
+    Command disabled;
 
+    CameraServer server;
+    
     public void robotInit() {
-    	OI.init();
-    	CommandBase.init();
-    }
+     RobotMap.init();
+     
+        OI.init();
+        CommandBase.init();  
+        
+        auntonselector = new SendableChooser();
+        auntonselector.addObject("aunton", new autontime() );
+        SmartDashboard.putData("aunton!", auntonselector);
 
-    public void teleopInit() {
-    	new Teleop().start();
-    }
-
-    public void teleopPeriodic() {
-        Scheduler.getInstance().run();
+        server = CameraServer.getInstance();
+        server.setQuality(50);
+        server.startAutomaticCapture("cam0");
     }
     
+ 
+ public void disabledPeriodic() {
+  Scheduler.getInstance().run();
+ }
+
     public void autonomousInit() {
     	
     }
 
+    public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+        
+    }
+
+    public void teleopInit() {
+     new TeleopDrive().start();
+    }
+
+    public void disabledInit(){
+
+    }
+
+    public void teleopPeriodic() {
+    	
+     Scheduler.getInstance().run();
+     
+    }
+
+    public void testPeriodic() {
+    }
+}
     public void autonomousPeriodic() {
     	
     }
