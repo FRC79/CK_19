4b1f5860523c25e924f1e8593c3d3f527cf0b932
Zach <zachnigels@gmail.com>
2016-01-22 20:47:25 -0500

    Putting subsystems into the commandBase

--- src/org/usfirst/frc/team79/robot/commands/CommandBase.java
+++ src/org/usfirst/frc/team79/robot/commands/CommandBase.java
@@ -1,15 +1,24 @@
 package org.usfirst.frc.team79.robot.commands;
 
+import org.usfirst.frc.team79.robot.OI;
 import org.usfirst.frc.team79.robot.subsystems.DriveTrain;
+import org.usfirst.frc.team79.robot.subsystems.FiringMechanism;
+import org.usfirst.frc.team79.robot.subsystems.IntakeMechanism;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 public abstract class CommandBase extends Command {
 	
 	public static DriveTrain driveTrain;	
+	public static OI oi;
+	public static IntakeMechanism intake;
+	public static FiringMechanism fire;
 	
 	public static void init() {
 		driveTrain = new DriveTrain();
+		intake = new IntakeMechanism();
+		oi = new OI();
+		fire = new FiringMechanism();
 	}
 
 	public CommandBase(String name) {
