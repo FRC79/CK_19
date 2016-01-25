432c29cfff899048d63166bc6f23b0bcd35e934f
Zach <zachnigels@gmail.com>
2016-01-25 18:36:24 -0500

    TankState
    State for driving tank

---
+++ src/org/usfirst/frc/team79/robot/commands/drivingstate/TankState.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc.team79.robot.commands.drivingstate;
+
+import org.usfirst.frc.team79.robot.OI;
+import org.usfirst.frc.team79.robot.commands.Drive;
+
+public class TankState implements State {
+	
+	boolean buttonToggle = false;
+	
+	Drive driveMechanism;
+
+	public TankState(Drive driveMechanism) {
+		this.driveMechanism = driveMechanism;
+	}
+	
+	@Override
+	public void execute() {
+		
+		if(OI.toggle.get()) {
+			buttonToggle = true;
+		}
+		
+		if(buttonToggle && !OI.toggle.get()) {
+			driveMechanism.setState(driveMechanism.getArcadeState());
+			buttonToggle = false;
+		} else {
+			driveMechanism.tankDrive(OI.getLeft(), OI.getRight());
+		}
+		
+	}
+
+}
