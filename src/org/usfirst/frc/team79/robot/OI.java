df0dde09f49c79f818e4e2c0de1a8dcbb54f2c67
ammhist <orionwolfe00@gmail.com>
2016-02-13 10:46:50 -0500

    Update OI.java

--- src/org/usfirst/frc/team79/robot/OI.java
+++ src/org/usfirst/frc/team79/robot/OI.java
@@ -2,31 +2,47 @@ package org.usfirst.frc.team79.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
+
 public class OI {
 	
-	public static Joystick left;
-	public static Joystick right;
-	public static JoystickButton arcButton;
+	public static boolean controllerToggle;
+	
+	public static Joystick left, right;
+	public static JoystickButton toggle, rotateArm, fire, WinchStart, Up, Drive, FireBall, Intake ;
 	
 	public static void init() {
 		
-		left = new Joystick(RobotMap.DRIVESTICKLEFT);
-		right = new Joystick(RobotMap.DRIVESTICKRIGHT);
-		arcButton = new JoystickButton(left, RobotMap.ARCBUTTON);
+		left = new Joystick(RobotMap.LEFTCONTROLLER);
+		right = new Joystick(RobotMap.RIGHTCONTROLLER);
+		toggle = new JoystickButton(left, 1);
+		rotateArm = new JoystickButton(left, 2);
+		fire = new JoystickButton(left, 3);
+		WinchStart = new JoystickButton(left, 5);
+		
+		Up = new JoystickButton(right,1);
+		Drive = new JoystickButton(right,2);
+		FireBall = new JoystickButton(right,3);
+		Intake = new JoystickButton(right,4);
 	}
-}
+	
+	public static double getLeft() {
+		return left.getY();
+	}
+	
+	public static double getRight() {
+		return right.getY();
+	}
 	
 	public static double getX() {
 		return left.getX();
 	}
 	
 	public static double getY() {
-		return left.getX();
+		return left.getY();
 	}
-
-}
 
